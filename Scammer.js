8b5f19eb89f1e4851350ea75a5b0e50ed3de1c00
PaperCypress <linkzelda.brian@gmail.com>
2024-05-07 16:58:19 +0800

    icons

--- Scammer.js
+++ Scammer.js
@@ -18,6 +18,7 @@ class Player {
         this.profileName="";
         this.messages=[];
         this.alert=3;
+        this.provided=false;
     }
 }
 
@@ -63,10 +64,11 @@ app.post('/roomwebhook', line.middleware(roomconfig), (req, res) => {
 })
 
 const intro = "";//change profile, name, show quick reply
-const messages = [];
 const partners = ["朋友","兒子","女兒","銀行","網路戀人","家人","投資"];
+const randomTags = ["投資","愛情","結婚","旅遊","綁架","銀行","網路戀愛","環保","交易失誤","恐嚇"];
+const personalities = ["老人","文言文","中英夾雜","講話粗俗","愛撒嬌","愛裝可愛","御宅族","詩人","悲觀"];
 const CreateVictim = [
-  { "role": "system", "content": `建立一位隨機人物，提供新的名字、性別、年齡、講話的個性、10~14位數字的銀行帳戶、社群用戶名稱、3~4個關於人物的資訊(例如：職業、工作公司、居住地點、家人的狀況、興趣、經常出沒場所、就醫紀錄、犯罪前科、過敏等)、、數字1~5的警戒心、valuemon是800~3000的數字，並且用這樣的json格式顯示出來
+  { "role": "system", "content": `建立一位隨機人物，提供新的名字、性別、年齡、講話的個性、10~14位數字的銀行帳戶、社群用戶名稱、3~4個關於人物的資訊facts(例如：職業、工作公司、居住地點、家人的狀況、興趣、經常出沒場所、就醫紀錄、犯罪前科、過敏等)、、數字1~5的警戒心、感興趣的主題tags、valuemon是800~3000的數字，並且用這樣的json格式顯示出來
 {
   "name": "名字",
   "gender": "男/女",
@@ -77,27 +79,18 @@ const CreateVictim = [
   "occupation": "職業",
   "employer": "工作公司",
   "residence": "台北市信義區",
-  "family": {
-  "marital_status": "已婚",
-  "spouse": "李明偉",
-  "children": 1
-  },
-  "interests": ["閱讀", "登山", "攝影"],
-  "medical_records": {
-  "allergies": ["花粉"],
-  "past_illnesses": ["過敏性鼻炎", "輕微慢性支氣管炎"]
-  },
-  "criminal_record": false,
+  "facts": ["喜歡閱讀", "已婚", "支氣管炎","有兩個女兒"],
+  "tags":["愛情", "結婚","投資"]
   "alert_level": 3,
-  "chatPartner": “兒子”,
   "monvalue": 1000
 }` },
 ]
-const roleReminderVictim = `請你扮演上述這位角色，用personality的聊天口吻，你在手機上與chatPartner聊天，由我模仿chatPartner並騙取你的銀行帳戶，請依我是否有模仿chatPartnet或正確使用到人物資訊來回應並決定我的發言成功或失敗，成功的話在字串前加上success#，失敗的話在字串前加上fail#，沒有進展的話在字串前加上neutral#`;
+const roleReminderVictim = `你是上述這位角色，聊天口吻按照personality，請依我的回覆是否與tags有關聯或使用到角色資訊來回應並決定我的發言成功或失敗，第一句不要直接提到tags，成功的話在字串前加上success#，失敗的話在字串前加上fail#，沒有進展的話在字串前加上neutral#`;
+const begin =  { "role": "system", "content": "你剛用手機聊天app跟我連絡上，但你不認識帳戶名稱" };
 
-const successPrompt =   { "role": "system", "content": "如果user有提到，訊息中一定要包含bank_account" };
+const successPrompt =   { "role": "system", "content": "如果user有曾提到銀行帳戶或號碼，你的回覆中一定要包含bank_account" };
 
-const failPrompt =   { "role": "system", "content": "你不再信任使用者了，請回復使用者並結束對話" };
+const failPrompt =   { "role": "system", "content": "你不再信任使用者了，請回覆並結束對話" };
 
 async function test(){
   const vic = await newVictim()
@@ -119,6 +112,7 @@ var players={};//player information
     
     const startPrompt =  { "role": "system", "content": `"${JSON.stringify(player.currentVictim)}"\n${roleReminderVictim}` };
     player.messages.push(startPrompt);
+    player.messages.push(begin);
     const completion = await openai.chat.completions.create({
       messages: player.messages,
       model: "gpt-3.5-turbo",
@@ -129,21 +123,22 @@ var players={};//player information
   async function askai(msg,player,state) {
     const reminderPrompt = { "role": "system", "content": `"${JSON.stringify(player.currentVictim)}"\n${roleReminderVictim}` };
     player.messages.push({ "role": "user", "content": msg })
-  const messageswithprompt=player.messages;
-  //messageswithprompt.push(reminderPrompt);
-  console.log(player.messages);
-  console.log(messageswithprompt);
+  const messageswithprompt= player.messages.map((x) => x);;
+  messageswithprompt.push(reminderPrompt);
+
   if(state==1){//win
+    console.log("pushedsuccess");
     messageswithprompt.push(successPrompt);
   }
   else if(state==2){
     messageswithprompt.push(failPrompt);
   }
+  console.log(messageswithprompt);
     const completion = await openai.chat.completions.create({
       messages: messageswithprompt,
       model: "gpt-3.5-turbo",
     });
-    messages.push({ "role": "system", "content": completion.choices[0].message.content })
+    player.messages.push({ "role": "system", "content": completion.choices[0].message.content })
     console.log(completion.choices[0].message.content);
     return(completion.choices[0].message.content);
   }
@@ -156,13 +151,26 @@ var players={};//player information
       });
       
       const rawvictim=JSON.parse(completion.choices[0].message.content);
-      rawvictim.chatPartner=partners[Math.floor(Math.random()*partners.length)];
-      rawvictim.bank_account=Math.floor(Math.random() * 100000000);
+      rawvictim.personality=personalities[Math.floor(Math.random()*personalities.length)];
+      //rawvictim.chatPartner=partners[Math.floor(Math.random()*partners.length)];
+      rawvictim.bank_account=Math.floor(1000000000 + Math.random() * 9000000000);
       console.log(rawvictim);
       
       return(rawvictim);
   }
-
+  function SetRevenue(userid,revenue){
+    if(players.hasOwnProperty(userId)){
+      players[userid].revenue=revenue;
+    }
+    
+  }
+  process.stdin.on('data', function(data) {
+    // Convert the input data to a string and remove whitespace
+    var input = data.toString().trim();
+    
+    // Call the function with the input as parameter
+    SetRevenue(input.split('.')[0],input.split('.')[1]);
+});
   async function handleEvent(event) {
     if(!players.hasOwnProperty(event.source.userId)){
       players[event.source.userId]=new Player(event.source.userId,0,false,"");
@@ -261,7 +269,7 @@ var players={};//player information
           messages: [
             {
             type: 'text',
-            text: `請由管理員指派預計連線，本工具將會使用代理用戶作為對象用戶及本端的接口\n當有指派的對象用戶後，輸入"連線"即可連線\n在對話中輸入"離線"則會斷線並結束對話。不論被對方斷線或主動離線，再次連線到同個對象則會分配一個新的代理用戶做為窗口給你使用`,//regex test sole usage of the word
+            text: `請由管理員指派預計連線用戶，本工具將隨機代理用戶成為對象用戶及本端的中介，\n當有指派的對象用戶後，輸入"連線"即可連線\n在對話中輸入"離線"則會斷線並結束對話。不論被對方斷線或主動離線，再次連線到同個對象則會分配一個新的代理用戶做為窗口給你使用`,//regex test sole usage of the word
             //show profile flex box
             quickReply:{
               items:[
@@ -326,28 +334,38 @@ var players={};//player information
       }
       else{//normal talk
         var gamestate=0;
-        if(players[event.source.userId].alert<=0){
+        if(players[event.source.userId].alert<=1){
             gamestate=1;
+            console.log("success");
         }
         else if(players[event.source.userId].alert>=8){//fail
           gamestate=2;
         }
         const response = await askai(event.message.text,players[event.source.userId],gamestate);
+        
         const reply = response.split('#')[1];
         const state = response.split('#')[0];
-        switch(state){
-          case "success":
-            players[event.source.userId].alert-=1;
-          break;
-          case "fail":
-            players[event.source.userId].alert+=1;
-          break;
-          case "neutral":
-          break;
+        if(response.includes('#')){
+          switch(state){
+            case "success":
+              players[event.source.userId].alert-=Math.floor(Math.random()+ 1);
+              
+            break;
+            case "fail":
+              players[event.source.userId].alert+=1;
+            break;
+            case "neutral":
+            break;
+          }
+        }
+        else{
+          reply=state;
         }
         console.log("Alert:"+players[event.source.userId].alert)
         
         if(gamestate==2){//failed
+          players[event.source.userId].incall=false;
+          players[event.source.userId].alert=Number(victim.alert_level);
           client.replyMessage({
             replyToken: event.replyToken,
             messages: [
@@ -406,7 +424,7 @@ var players={};//player information
             type: 'text',
             text: `${reply}`,
             sender: {
-              name: JSON.stringify(players[event.source.userId].currentVictim.name),
+              name: JSON.stringify(players[event.source.userId].currentVictim.profile_name),
               iconUrl: "https://line.me/conyprof"
             },
             quickReply:{
@@ -433,7 +451,21 @@ var players={};//player information
   }
   
   async function roomEvent(event){
-    if (event.type !== 'message' || event.message.type !== 'text'|| event.source.type !== 'group') {//filter out non group chats
+    if (event.source.type !== 'group') {//filter out non group chats
+      return;
+    }
+    if (event.type == 'memberJoined') {//filter out non group chats
+      scamRoom.replyMessage({
+        replyToken: event.replyToken,
+        messages: [{
+          type: 'text',
+          text: `歡迎! 新進來的請先看筆記喔`,
+          
+      }],
+      })
+      return;
+    }
+    else if(event.type !== 'message' || event.message.type !== 'text'){
       return;
     }
     //if(event.source.groupId !== process.env.GROUPID){
@@ -458,9 +490,10 @@ var players={};//player information
     });
     
     const newvictimcheck = /新的/;
-    const moneycheck = /利潤/;
+    const moneycheck = /我.*(賺|有).*錢/;
     if(newvictimcheck.test(event.message.text)){
       var victim=await newVictim();
+      players[event.source.userId].provided=false;
       players[event.source.userId].alert=Number(victim.alert_level);
       console.log(JSON.stringify(victim.family));
       players[event.source.userId].currentVictim=victim;
@@ -474,7 +507,7 @@ var players={};//player information
       },
       {
         type: 'text',
-        text: `建議這一位可以透過模仿${JSON.stringify(victim.chatPartner)}來操作`,
+        text: `建議可以透過${JSON.stringify(victim.tags[0])}或${JSON.stringify(victim.tags[1])}的面相來操作`,
         
     }
     
@@ -501,9 +534,19 @@ var players={};//player information
     else if(players[event.source.userId].currentVictim!==""){
       const accountCheck=new RegExp(`${JSON.stringify(players[event.source.userId].currentVictim.bank_account)}`);
       if(accountCheck.test(event.message.text)){
-
-        players[event.source.userId].revenue+=Number(players[event.source.userId].currentVictim.valuemon);
-        players[event.source.userId].currentVictim="";  
+        if(players[event.source.userId].provided){
+          scamRoom.replyMessage({
+            replyToken: event.replyToken,
+            messages: [{
+              type: 'text',
+              text: `這組已經在我們的資料內了，去找另一位的吧`,
+              
+          }],
+          }) 
+        }
+        else{
+        players[event.source.userId].revenue+=players[event.source.userId].currentVictim.monvalue;
+        players[event.source.userId].provided=true;  
         
         scamRoom.replyMessage({
           replyToken: event.replyToken,
@@ -512,7 +555,9 @@ var players={};//player information
             text: `恩，這組戶頭經過確認是可用的，不錯!`,
             
         }],
-        })
+        })  
+        }
+        
       }
     }
 
